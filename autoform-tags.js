e6737d8cfea373356b2d2ca66380736dc2fafb83
jankapunkt <jkuester@uni-bremen.de>
2018-05-24 15:21:03 +0200

    repo created

---
+++ autoform-tags.js
@@ -0,0 +1,5 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See autoform-tags-tests.js for an example of importing.
+export const name = 'autoform-tags';
