5bad74be2c76c65b1bcd954826c343d899f2bf6b
jankapunkt <jkuester@uni-bremen.de>
2018-07-24 16:17:11 +0200

    code formatted in standard style

--- autoform-tags.js
+++ autoform-tags.js
@@ -1,322 +1,318 @@
-import { Template } from 'meteor/templating';
-import { ReactiveDict } from 'meteor/reactive-dict';
-import { $ } from 'meteor/jquery';
+/* global AutoForm */
+import { Template } from 'meteor/templating'
+import { ReactiveDict } from 'meteor/reactive-dict'
+import { $ } from 'meteor/jquery'
 
-import './autoform-tags.css';
-import './autoform-tags.html';
+import './autoform-tags.css'
+import './autoform-tags.html'
 
 // extend autoform with bpmn modeler
 AutoForm.addInputType('tags', {
   template: 'afTags',
-  valueOut() {
-    const val = this.val();
-    return val && JSON.parse(val);
+  valueOut () {
+    const val = this.val()
+    return val && JSON.parse(val)
   },
-  valueIn(initialValue) {
-    return initialValue;
-  },
-});
+  valueIn (initialValue) {
+    return initialValue
+  }
+})
 
 Template.afTags.onCreated(function () {
-  const instance = this;
-  instance.state = new ReactiveDict();
-  instance.state.set('target', null);
-  instance.state.set('value', []);
-  instance.state.set('double', null);
-  instance.state.set('showPlaceholder', true);
+  const instance = this
+  instance.state = new ReactiveDict()
+  instance.state.set('target', null)
+  instance.state.set('value', [])
+  instance.state.set('double', null)
+  instance.state.set('showPlaceholder', true)
 
   instance.autorun(function () {
-    const { data } = instance;
-    const { atts } = data;
+    const {data} = instance
+    const {atts} = data
 
     // create dict of select options
-    let optionsMap;
+    let optionsMap
     if (data.selectOptions) {
-      optionsMap = {};
+      optionsMap = {}
       data.selectOptions.forEach((entry) => {
-        optionsMap[entry] = true;
-      });
+        optionsMap[entry] = true
+      })
     }
 
-    instance.state.set('minLength', atts.min || -1);
-    instance.state.set('maxLength', atts.max || -1);
-    instance.state.set('selectOptions', data.selectOptions);
-    instance.state.set('optionsMap', optionsMap);
-    instance.state.set('placeholder', atts.placeholder || '...');
-    instance.state.set('onlyOptions', !!atts.onlyOptions);
-    instance.state.set('dataSchemaKey', atts['data-schema-key']);
+    instance.state.set('minLength', atts.min || -1)
+    instance.state.set('maxLength', atts.max || -1)
+    instance.state.set('selectOptions', data.selectOptions)
+    instance.state.set('optionsMap', optionsMap)
+    instance.state.set('placeholder', atts.placeholder || '...')
+    instance.state.set('onlyOptions', !!atts.onlyOptions)
+    instance.state.set('dataSchemaKey', atts['data-schema-key'])
 
-    const { value } = data;
+    const {value} = data
     if (value && instance.state.get('value').length === 0) {
-      $('#afTags-hiddenInput').val(JSON.stringify(value));
-      instance.state.set('value', value);
+      $('#afTags-hiddenInput').val(JSON.stringify(value))
+      instance.state.set('value', value)
     }
 
-    instance.state.set('loadComplete', true);
-  });
-});
+    instance.state.set('loadComplete', true)
+  })
+})
 
 Template.afTags.helpers({
-  loadComplete() {
-    return Template.instance().state.get('loadComplete');
+  loadComplete () {
+    return Template.instance().state.get('loadComplete')
   },
-  dataSchemaKey() {
-    return Template.instance().state.get('dataSchemaKey');
+  dataSchemaKey () {
+    return Template.instance().state.get('dataSchemaKey')
   },
-  isSelected(index) {
-    return Template.instance().state.get('target') === index;
+  isSelected (index) {
+    return Template.instance().state.get('target') === index
   },
-  tags() {
-    return Template.instance().state.get('value');
+  tags () {
+    return Template.instance().state.get('value')
   },
-  onlyOptions() {
-    return Template.instance().state.get('onlyOptions');
+  onlyOptions () {
+    return Template.instance().state.get('onlyOptions')
   },
-  showSelectOptions() {
-    return Template.instance().state.get('selectOptions') && Template.instance().state.get('showSelectOptions');
+  showSelectOptions () {
+    return Template.instance().state.get('selectOptions') && Template.instance().state.get('showSelectOptions')
   },
-  selectOptions() {
-    return Template.instance().state.get('selectOptions');
+  selectOptions () {
+    return Template.instance().state.get('selectOptions')
   },
-  selected(tag) {
-    const value = Template.instance().state.get('value');
-    return value && value.indexOf(tag) > -1;
+  selected (tag) {
+    const value = Template.instance().state.get('value')
+    return value && value.indexOf(tag) > -1
   },
-  showPlaceholder() {
-    return Template.instance().state.get('placeholder') && Template.instance().state.get('showPlaceholder');
+  showPlaceholder () {
+    return Template.instance().state.get('placeholder') && Template.instance().state.get('showPlaceholder')
   },
-  placeholder() {
-    return Template.instance().state.get('placeholder');
+  placeholder () {
+    return Template.instance().state.get('placeholder')
   },
-  isDouble(index) {
-    const double = Template.instance().state.get('double');
-    return double > -1 && double === index;
+  isDouble (index) {
+    const double = Template.instance().state.get('double')
+    return double > -1 && double === index
   },
-  hasLimits() {
-    return Template.instance().state.get('minLength') > -1 || Template.instance().state.get('maxLength') > -1;
+  hasLimits () {
+    return Template.instance().state.get('minLength') > -1 || Template.instance().state.get('maxLength') > -1
   },
-  minLength() {
-    const len = Template.instance().state.get('minLength');
-    return len > -1 ? len : null;
+  minLength () {
+    const len = Template.instance().state.get('minLength')
+    return len > -1 ? len : null
   },
-  maxLength() {
-    const len = Template.instance().state.get('maxLength');
-    return len > -1 ? len : null;
+  maxLength () {
+    const len = Template.instance().state.get('maxLength')
+    return len > -1 ? len : null
   },
-  limitsExceeded() {
-    return Template.instance().state.get('limitsExceeded');
-  },
-});
+  limitsExceeded () {
+    return Template.instance().state.get('limitsExceeded')
+  }
+})
 
-function getTag(text = '') {
-  return text.replace(/\s\s+/g, ' ').trim();
+function getTag (text = '') {
+  return text.replace(/\s\s+/g, ' ').trim()
 }
 
-function applyInput({ templateInstance }) {
-  const input = $('#aftags-input');
-  const index = parseInt(input.attr('data-index'), 10);
-  const target = templateInstance.state.get('target');
-  const value = templateInstance.state.get('value');
-  const tag = getTag(input.text());
+function applyInput ({templateInstance}) {
+  const input = $('#aftags-input')
+  const index = parseInt(input.attr('data-index'), 10)
+  const target = templateInstance.state.get('target')
+  const value = templateInstance.state.get('value')
+  const tag = getTag(input.text())
 
-
-  const doubleIndex = templateInstance.state.get('double');
-  if (doubleIndex > -1 && doubleIndex !== index) return;
+  const doubleIndex = templateInstance.state.get('double')
+  if (doubleIndex > -1 && doubleIndex !== index) return
 
   if (tag.length === 0 || tag === '') {
-    return;
+    return
   }
 
-  const minLength = templateInstance.state.get('minLength');
+  const minLength = templateInstance.state.get('minLength')
   if (minLength > -1 && tag.length < minLength) {
-    templateInstance.state.set('limitsExceeded', true);
-    return;
+    templateInstance.state.set('limitsExceeded', true)
+    return
   }
 
-  const maxLength = templateInstance.state.get('maxLength');
+  const maxLength = templateInstance.state.get('maxLength')
   if (maxLength > -1 && tag.length > maxLength) {
-    templateInstance.state.set('limitsExceeded', true);
-    return;
+    templateInstance.state.set('limitsExceeded', true)
+    return
   }
 
-  const onlyOptions = templateInstance.state.get('onlyOptions');
-  const optionsMap = templateInstance.state.get('optionsMap');
+  const onlyOptions = templateInstance.state.get('onlyOptions')
+  const optionsMap = templateInstance.state.get('optionsMap')
 
   if (onlyOptions && optionsMap && !optionsMap[tag]) {
     // TODO show err msg
-    return;
+    return
   }
-
 
   if (index === -1) {
-    value.push(tag);
+    value.push(tag)
   } else if (index === target) {
-    value[index] = tag;
+    value[index] = tag
   } else {
-    value.splice(index, 0, tag);
+    value.splice(index, 0, tag)
   }
 
-  $('#afTags-hiddenInput').val(JSON.stringify(value));
-  templateInstance.state.set('value', value);
-  templateInstance.state.set('target', null);
-  input.text('');
+  $('#afTags-hiddenInput').val(JSON.stringify(value))
+  templateInstance.state.set('value', value)
+  templateInstance.state.set('target', null)
+  input.text('')
 
   setTimeout(() => {
-    $('#aftags-input').focus();
-  }, 30);
+    $('#aftags-input').focus()
+  }, 30)
 }
 
 Template.afTags.events({
 
-  'focus #aftags-input'(event, templateInstance) {
-    event.preventDefault();
-    templateInstance.state.set('showSelectOptions', true);
-    templateInstance.state.set('showPlaceholder', false);
+  'focus #aftags-input' (event, templateInstance) {
+    event.preventDefault()
+    templateInstance.state.set('showSelectOptions', true)
+    templateInstance.state.set('showPlaceholder', false)
   },
 
-  'blur #aftags-input'(event, templateInstance) {
+  'blur #aftags-input' (event, templateInstance) {
     // event.preventDefault();
     // event.stopPropagation();
 
-
     // cancel on blur
-    const input = $(event.currentTarget);
-    const index = parseInt(input.attr('data-index'), 10);
-    const target = templateInstance.state.get('target');
+    const input = $(event.currentTarget)
+    const index = parseInt(input.attr('data-index'), 10)
+    const target = templateInstance.state.get('target')
     if (index === target) {
-      templateInstance.state.set('target', null);
+      templateInstance.state.set('target', null)
     }
-    const tagLength = input.text().length;
-    templateInstance.state.set('showSelectOptions', tagLength !== 0);
-    templateInstance.state.set('showPlaceholder', tagLength === 0);
+    const tagLength = input.text().length
+    templateInstance.state.set('showSelectOptions', tagLength !== 0)
+    templateInstance.state.set('showPlaceholder', tagLength === 0)
   },
 
-  'click #aftags-input-showenterbutton'(event) {
-    event.preventDefault();
-    $('#aftags-input').focus();
+  'click #aftags-input-showenterbutton' (event) {
+    event.preventDefault()
+    $('#aftags-input').focus()
   },
 
-  'click #aftags-input-applybutton'(event, templateInstance) {
-    event.preventDefault();
-    applyInput({ templateInstance });
+  'click #aftags-input-applybutton' (event, templateInstance) {
+    event.preventDefault()
+    applyInput({templateInstance})
   },
 
-  'input #aftags-input'(event, templateInstance) {
+  'input #aftags-input' (event, templateInstance) {
     // detect the current input and immediately
     // cache it to be a double if true
-    const input = $(event.target);
-    const tag = getTag(input.text());
-    const index = parseInt(input.attr('data-index'), 10);
-    const value = templateInstance.state.get('value');
+    const input = $(event.target)
+    const tag = getTag(input.text())
+    const index = parseInt(input.attr('data-index'), 10)
+    const value = templateInstance.state.get('value')
 
-
-    const minLength = templateInstance.state.get('minLength');
+    const minLength = templateInstance.state.get('minLength')
     if (minLength > -1 && tag.length < minLength) {
-      templateInstance.state.set('limitsExceeded', true);
-      return;
+      templateInstance.state.set('limitsExceeded', true)
+      return
     }
 
-    const maxLength = templateInstance.state.get('maxLength');
+    const maxLength = templateInstance.state.get('maxLength')
     if (maxLength > -1 && tag.length > maxLength) {
-      templateInstance.state.set('limitsExceeded', true);
-      return;
+      templateInstance.state.set('limitsExceeded', true)
+      return
     }
 
-    templateInstance.state.set('limitsExceeded', false);
+    templateInstance.state.set('limitsExceeded', false)
 
     // if there is a double found
     // and it's index is not the current index
     // which may be the case if we edit the
     // current tag
-    const doubleIndex = value.indexOf(tag);
-    const isDouble = doubleIndex > -1 && doubleIndex !== index;
-    templateInstance.state.set('double', isDouble ? doubleIndex : -1);
+    const doubleIndex = value.indexOf(tag)
+    const isDouble = doubleIndex > -1 && doubleIndex !== index
+    templateInstance.state.set('double', isDouble ? doubleIndex : -1)
   },
 
-  'keydown #aftags-input'(event, templateInstance) {
+  'keydown #aftags-input' (event, templateInstance) {
     // if typing... return
     if (event.key !== 'Enter' && event.key !== 'Escape' && event.key !== 'Tab') {
-      return;
+      return
     }
 
-    const input = $(event.target);
-    const index = parseInt(input.attr('data-index'), 10);
+    const input = $(event.target)
+    const index = parseInt(input.attr('data-index'), 10)
 
-    event.preventDefault();
-    event.stopPropagation();
-
+    event.preventDefault()
+    event.stopPropagation()
 
     // ESC -> cancel
     if (event.key === 'Escape') {
-      templateInstance.state.set('target', null);
-      templateInstance.state.set('double', -1);
+      templateInstance.state.set('target', null)
+      templateInstance.state.set('double', -1)
 
       if (index === -1) {
-        input.text('');
+        input.text('')
       }
 
-      input.blur();
-      return;
+      input.blur()
+      return
     }
 
-    applyInput({ templateInstance });
+    applyInput({templateInstance})
   },
 
-  'click .aftags-close'(event, templateInstance) {
-    event.preventDefault();
-    event.stopPropagation();
+  'click .aftags-close' (event, templateInstance) {
+    event.preventDefault()
+    event.stopPropagation()
     // delete tag
 
-    const input = $(event.currentTarget);
-    const doubleIndex = templateInstance.state.get('double');
-    const index = parseInt(input.attr('data-index'), 10);
+    const input = $(event.currentTarget)
+    const doubleIndex = templateInstance.state.get('double')
+    const index = parseInt(input.attr('data-index'), 10)
 
     // if item to delete
     // is stored in double's cache
     if (doubleIndex === index) {
-      templateInstance.state.set('double', -1);
+      templateInstance.state.set('double', -1)
     }
 
-    const value = templateInstance.state.get('value');
-    value.splice(index, 1);
+    const value = templateInstance.state.get('value')
+    value.splice(index, 1)
 
-    $('#afTags-hiddenInput').val(JSON.stringify(value));
-    templateInstance.state.set('value', value);
+    $('#afTags-hiddenInput').val(JSON.stringify(value))
+    templateInstance.state.set('value', value)
 
     setTimeout(() => {
-      $('#aftags-input').focus();
-    }, 30);
+      $('#aftags-input').focus()
+    }, 30)
   },
 
-  'click .aftags-tag'(event, templateInstance) {
-    event.preventDefault();
-    event.stopPropagation();
+  'click .aftags-tag' (event, templateInstance) {
+    event.preventDefault()
+    event.stopPropagation()
 
     // edit tag
-    const index = $(event.currentTarget).attr('data-index');
-    templateInstance.state.set('target', parseInt(index, 10));
-    templateInstance.state.set('showSelectOptions', true);
-    templateInstance.state.set('showPlaceholder', false);
+    const index = $(event.currentTarget).attr('data-index')
+    templateInstance.state.set('target', parseInt(index, 10))
+    templateInstance.state.set('showSelectOptions', true)
+    templateInstance.state.set('showPlaceholder', false)
 
     setTimeout(() => {
-      $('#aftags-input').focus();
-    }, 30);
+      $('#aftags-input').focus()
+    }, 30)
   },
 
-  'mousedown .aftags-option'(event, templateInstance) {
-    event.preventDefault();
-    event.stopPropagation();
+  'mousedown .aftags-option' (event, templateInstance) {
+    event.preventDefault()
+    event.stopPropagation()
 
-    const input = $(event.currentTarget);
-    const target = input.attr('data-target');
-    const value = templateInstance.state.get('value');
+    const input = $(event.currentTarget)
+    const target = input.attr('data-target')
+    const value = templateInstance.state.get('value')
 
-    value.push(target);
+    value.push(target)
 
-    input.text('');
-    $('#afTags-hiddenInput').val(JSON.stringify(value));
-    templateInstance.state.set('value', value);
-    templateInstance.state.set('target', null);
-    templateInstance.state.set('showSelectOptions', true);
-  },
-});
+    input.text('')
+    $('#afTags-hiddenInput').val(JSON.stringify(value))
+    templateInstance.state.set('value', value)
+    templateInstance.state.set('target', null)
+    templateInstance.state.set('showSelectOptions', true)
+  }
+})
--- package.js
+++ package.js
@@ -1,3 +1,5 @@
+/* global Package */
+
 Package.describe({
   name: 'jkuester:autoform-tags',
   version: '0.2.0',
@@ -7,30 +9,30 @@ Package.describe({
   git: 'https://github.com/jankapunkt/meteor-autoform-tags.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md',
-});
+  documentation: 'README.md'
+})
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.6');
+  api.versionsFrom('1.6')
 
   api.use([
     'ecmascript',
     'reactive-dict',
     'templating@1.3.2',
     'aldeed:autoform@6.0.0',
-    'aldeed:template-extension@4.0.0',
-  ]);
+    'aldeed:template-extension@4.0.0'
+  ])
 
   api.addFiles([
     'autoform-tags.css',
     'autoform-tags.html',
-    'autoform-tags.js',
-  ], 'client');
-});
+    'autoform-tags.js'
+  ], 'client')
+})
 
 Package.onTest(function (api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('jkuester:autoform-tags');
-  api.mainModule('autoform-tags-tests.js');
-});
+  api.use('ecmascript')
+  api.use('tinytest')
+  api.use('jkuester:autoform-tags')
+  api.mainModule('autoform-tags-tests.js')
+})
