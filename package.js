9e380acda530dfdf576b51f1e3074f1a98574c0c
Jan Küster <jkuester@uni-bremen.de>
2018-11-12 15:29:51 +0100

    released 1.0.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'jkuester:autoform-tags',
-  version: '0.2.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Provides a form field to enter tags.',
   // URL to the Git repository containing the source code for this package.
